b6a4692fc8a2fae621f93733f8bb312f1ad1d620
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2015-08-01 16:37:08 +0200

    fix(ci): Changed informations for auto deploy and changed the url to git repo.

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
 
 git checkout master
-git config remote.origin.url "$GH_TOKEN@github.com:juanwolf/mysql-binlog-replicator.git"
+git remote rm origin
+git remote add origin "https://juanwolf:$GH_TOKEN@github.com/juanwolf/mysql-binlog-replicator.git"
 
 mvn -s ./.travis.settings.xml  --batch-mode -Dtag=$TAG -DreleaseVersion=$VERSION -Dgpg.passphrase=$GPG_PASSPHRASE release:prepare release:clean
 mvn -s ./.travis.settings.xml release:perform -Dgpg.passphrase=$GPG_PASSPHRASE
